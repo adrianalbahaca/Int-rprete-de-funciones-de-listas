19a72e477f98072c884f8a22c175896a021da8f8
Adrian <adrian.albahaca@gmail.com>
2025-07-29 16:01:21 +0000

    Principios de AST iniciados. Por ahora, falta buscar una forma de ir armando el AST poco a poco, y eliminarlo si algo sale mal

--- ast.c
+++ ast.c
@@ -9,6 +9,15 @@ static TokenNodo* lista;
 /* ------ Funciones auxiliares ------ */
 
 /**
+ * error: void -> void
+ * Si se invoca, avisa que hay un error sintáctico en lo escrito
+ */
+void error() {
+    printf("Error de sintáxis. Prueba de nuevo\n");
+    exit(1);
+}
+
+/**
  * next: TokenNodo* -> void
  * Si se ingresa una lista simplemente enlazada de tokens, se guarda la lista. Sino, se mueve
  * al siguiente token de una lista ya guardada anteriormente
@@ -22,6 +31,15 @@ void next(TokenNodo* tokens) {
         lista = lista->sig;
     }
 }
+
+/**
+ * match: TipoDeToken -> void
+ * Si el token buscado coincide con el primer token de lista, invoca a next. Sino, salta un error
+ */
+void match(TipoDeToken tipo) {
+    if (tipo == lista->tipo) next(NULL);
+    else error();
+}
 /* ---------------------------------- */
 
 /**
@@ -33,4 +51,30 @@ ASTTree parse(TokenList tokens) {
     /* Primero, voy a inicializar la lista en la función next */
     next(tokens->head);
     
+    // Luego, se inicia el parser
+    ASTTree tree = NULL;
+
+    switch (lista->tipo) {
+        case TOKEN_DEFF:
+            tree = Deff();
+            match(TOKEN_PUNTO_COMA);
+            break;
+        case TOKEN_DEFL:
+            tree = Defl();
+            match(TOKEN_PUNTO_COMA);
+            break;
+        case TOKEN_APPLY:
+            tree = Apply();
+            match(TOKEN_PUNTO_COMA);
+            break;
+        case TOKEN_SEARCH:
+            tree = Search();
+            match(TOKEN_PUNTO_COMA);
+            break;
+        default:
+            error();
+            break;
+    }
+
+    return tree;
 }
