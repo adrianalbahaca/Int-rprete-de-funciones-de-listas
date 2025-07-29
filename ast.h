51377f044b166c8a20e633fbc7a027a6339b7223
Adrian <adrian.albahaca@gmail.com>
2025-07-29 15:50:12 +0000

    Reinicio del AST

--- ast.h
+++ ast.h
@@ -3,142 +3,33 @@
 #include "tokenizer.h"
 
 /**
- * TODO: Diseñar los nodos del AST. Son los siguientes:
- * 1. Nodos de la sentencia: Busqueda, Aplicacion y Definicion
- * 2. Nodos hoja (Terminales): DEF, DIGITO, PRIMITIVA
- * 3. Nodos intermedios (No terminales): FUNCION, FUNCIONES, LISTA, LISTAS, etc
+ * Se define los tipos de nodos para el AST
  */
 
-/**
- * Primero, determino los tipos de nodos que pueden haber
- */
 typedef enum {
+    /* Estos son los tipos de nodo raíz */
+    AST_SENTENCIA, AST_DEFF, AST_DEFL, AST_APPLY, AST_SEARCH,
 
-    // Primero, se define los tipos de nodo raíz
+    /* Estos son tipos de nodo internos */
+    AST_FUNCS, AST_FUNC, AST_REP, AST_LISTAS, AST_LISTA, AST_ELEMENTOS,
 
-    DEFF_FUNCION, // Definicion := 'deff' DEF '=' FUNCIONES | 'deff' DEF '=' REPETICION
-    DEFL_LISTA, // 'defl' DEF '=' '[' ELEMENTOS ']'
-    APPLY, // 'apply' FUNCION LISTA
-    SEARCH, // 'search' '{' LISTAS '}'
-
-    // Luego, se define los nodos hoja
-
-    DEF, // String a asociar a una lista
-    PRIMITIVA, // PRIMITIVA := '0i' | '0d' | 'Si' | 'Sd' | 'Di' | 'Dd'
-    DIGITO, // Número entero
-
-    // Tipos de Sentencia
-
-    LISTAS, // Listas ::= Lista COMA Lista PUNTO_COMA Listas | Lista COMA Lista
-    LISTA, // Lista ::= DEF | COR_ABRE Elementos COR_CIERRA
-    ELEMENTOS, // 
-    REPETICION,
-    FUNCS,
-
-    // Por último, el comando de cierre
-    
-    QUIT
-
+    /* Esto son tipos de nodos hoja */
+    AST_DEF, AST_PRIMITIVA, AST_DIGITO
 } TipoDeNodo;
 
 /**
- * Luego, defino las funciones primitivas
- */
-typedef enum {
-    CERO_IZQ, 
-    CERO_DER,
-    SUC_IZQ,
-    SUC_DER,
-    DEL_IZQ,
-    DEL_DER
-} TipoPrimitiva;
-
-/**
- * Se define la estructura de los nodos hoja
- */
-
-/**
- * Esta es la estructura para el nodo hoja de tipo DEF
+ * Se define la estructura general de un nodo de un árbol. Tiene un máx. de
+ * 3 hijos cada uno
  */
 typedef struct {
-    String def;
-} NodoDef;
-
-/**
- * Esta es la estructura para el nodo hoja de tipo DIGITO
- */
-typedef struct {
-    int digito;
-} NodoDigito;
-
-typedef struct {
-    TipoPrimitiva primitiva;
-} NodoPrimitiva;
-
-typedef struct {
-    int quit;
-} NodoQuit;
-
-/**
- * Se define los nodos raíces
- */
-typedef struct {
-    ASTNodo* def;
-    ASTNodo* funcs;
-} NodoDeff;
-
-typedef struct {
-    ASTNodo* function;
-    ASTNodo* lista;
-} NodoDefl;
-
-typedef struct {
-    ASTNodo* function;
-    ASTNodo* list;
-} NodoApply;
-
-/**
- * Además, se define los nodos intermedios
- */
-
-typedef struct {
-    ASTNodo* funcion;
-    ASTNodo* funciones;
-} NodoFunciones;
-
-/**
- * Finalmente, una unión masiva donde se guarda cada tipo
- */
-typedef union {
-
-    // Nodos hoja
-
-    NodoDef Def;
-    NodoDigito Digito;
-    NodoPrimitiva Primitiva;
-
-    // Nodos raíz
-    NodoDeff Deff;
-    NodoDefl Defl;
-    NodoApply Apply;
-    NodoSearch Search;
-
-    // Nodos intermedios
-    NodoFunciones Funciones;
-
-    // Comando quit
-    NodoQuit Quit;
-
-
-} DatosNodo;
-
-/**
- * Esta es la definición del nodo general
- */
-typedef struct _ASTNodo {
     TipoDeNodo tipo;
-    DatosNodo datos; 
+    String lexema;
+    struct ASTNodo* hijos[3];
 } ASTNodo;
+
+/**
+ * También, por conveniencia, se define un nombre para el nodo raíz del AST
+ */
 
 typedef ASTNodo* ASTTree;
 
